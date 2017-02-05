3a9a906d6883e5bbadd9c0d79002c641fcbe739d
georgidinolov <gdinolov@soe.ucsc.edu>
2017-02-04 22:04:22 -0800

    parallel check solver

--- igraph-0.7.1/examples/simple/check-sparse-solver.cpp
+++ igraph-0.7.1/examples/simple/check-sparse-solver.cpp
@@ -3,73 +3,98 @@ extern "C" {
 #include "igraph_sparsemat.h"
 }
 
+#include <omp.h>
 #include <vector>
 
 int main() {
 
-  long int i, j;
-  int dimension = 1000;
+  long int i;
+  int j;
+  int dimension = 10000;
   
   /***********************************************************************/
-  int n_eigenproblems = 2;
-  // std::vector<igraph_sparsemat_t> system_mats (n_eigenproblems);
-
-  igraph_sparsemat_t A, B;
-  igraph_matrix_t vectors;
-  igraph_vector_t values;
-  igraph_arpack_options_t options;
+  int n_eigenproblems = 8;
+  std::vector<igraph_sparsemat_t> system_mats_A (n_eigenproblems);
+  std::vector<igraph_sparsemat_t> system_mats_B (n_eigenproblems);
   
   igraph_rng_seed(igraph_rng_default(), 42 * 42);
-  
-  igraph_sparsemat_init(&A, dimension, dimension,
-			dimension + 2*(dimension-1));
 
   for (j=0; j<n_eigenproblems; j++) {
+    igraph_sparsemat_init(&system_mats_A[j], dimension, dimension,
+  			  dimension + 2*(dimension-1));
+
     for (i=0; i<dimension; i++) {
-      igraph_sparsemat_entry(&A, i, i,
-			     igraph_rng_get_integer(igraph_rng_default(),
-						    -10, 10));
+      igraph_sparsemat_entry(&system_mats_A[j], i, i,
+  			     igraph_rng_get_integer(igraph_rng_default(),
+  						    -10, 10));
       if (i > 0) {
-	int off_diagonal_entry = igraph_rng_get_integer(igraph_rng_default(),
-							-10, 10);
-	igraph_sparsemat_entry(&A, i-1, i, off_diagonal_entry);
-	igraph_sparsemat_entry(&A, i, i-1, off_diagonal_entry);
+  	int off_diagonal_entry = igraph_rng_get_integer(igraph_rng_default(),
+  							-10, 10);
+  	igraph_sparsemat_entry(&system_mats_A[j], i-1, i, off_diagonal_entry);
+  	igraph_sparsemat_entry(&system_mats_A[j], i, i-1, off_diagonal_entry);
       }
     }
+    
+    igraph_sparsemat_compress(&system_mats_A[j], &system_mats_B[j]);
+    igraph_sparsemat_destroy(&system_mats_A[j]);
   }
-  igraph_sparsemat_compress(&A, &B);
-  igraph_sparsemat_destroy(&A);
-  
-  igraph_arpack_options_init(&options);
-  options.n = dimension;
-  options.nev = 150;
-  options.ncv = 0;
-  options.which[0] = 'L';
-  options.which[1] = 'M';
-  options.mode = 1;
-  options.sigma = 3;
-  options.tol = 1e-16;
-  
-  igraph_vector_init(&values, options.nev);
-  igraph_matrix_init(&vectors, options.n, 0);
-  
-  printf("options.which = %s\n", options.which); 
 
-  igraph_arpack_storage_t storage;
-  igraph_arpack_storage_init(&storage, dimension, dimension, dimension, true);
+  omp_set_dynamic(0);
+#pragma omp parallel shared(system_mats_B)
+  {
+#pragma omp for      
+    for (j=0; j<n_eigenproblems; j++) {
+      igraph_matrix_t vectors;
+      igraph_vector_t values;
+      igraph_arpack_options_t options;
+      
+      igraph_arpack_options_init(&options);
+      options.n = dimension;
+      options.nev = 10;
+      options.ncv = 0;
+      options.which[0] = 'L';
+      options.which[1] = 'M';
+      options.mode = 1;
+      options.sigma = 3;
+      options.tol = 1e-16;
+      
+      igraph_vector_init(&values, options.nev);
+      igraph_matrix_init(&vectors, options.n, 0);
+      
+      printf("options.which = %s\n", options.which); 
+      
+      igraph_arpack_storage_t storage;
+      igraph_arpack_storage_init(&storage, dimension, dimension, dimension, true);
+      
+      
+      igraph_sparsemat_arpack_rssolve(&system_mats_B[j], &options, &storage,
+				      &values, &vectors,
+				      IGRAPH_SPARSEMAT_SOLVE_LU);
+      /* if (VECTOR(values)[0] != 1.0) { return 1; } */
+      
+      igraph_vector_print(&values); 
+      
+      igraph_vector_destroy(&values);
+      igraph_matrix_destroy(&vectors);
+      igraph_arpack_storage_destroy(&storage);
+      }
+  } // END PARALLEL REGION
+  
+  for (int j=0; j<n_eigenproblems; j++) {
+    igraph_sparsemat_destroy(&system_mats_B[j]);
+  }
 
-  
-  igraph_sparsemat_arpack_rssolve(&B, &options, &storage,
-				  &values, &vectors,
-				  IGRAPH_SPARSEMAT_SOLVE_LU);
-  /* if (VECTOR(values)[0] != 1.0) { return 1; } */
-  
-  igraph_vector_print(&values); 
-  
-  igraph_vector_destroy(&values);
-  igraph_matrix_destroy(&vectors);
-  igraph_sparsemat_destroy(&B);
-  igraph_arpack_storage_destroy(&storage);
-  
+  omp_set_dynamic(0);
+#pragma omp parallel private(j)
+  {
+#pragma omp master
+    {
+      printf("Master construct is executed by thread %d\n",
+	     omp_get_thread_num());
+      
+      printf("There are %d max threads\n",
+	     omp_get_max_threads());
+    }
+  }
   return 0;
 }
