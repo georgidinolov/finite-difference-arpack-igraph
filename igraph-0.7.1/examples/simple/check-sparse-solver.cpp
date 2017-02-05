1b5839d7bb4fec3d9e05cabe1ee89261ead8ec3a
georgidinolov <gdinolov@soe.ucsc.edu>
2017-02-04 20:20:34 -0800

    adding build env

---
+++ igraph-0.7.1/examples/simple/check-sparse-solver.cpp
@@ -0,0 +1,75 @@
+extern "C" {
+#include "igraph.h"
+#include "igraph_sparsemat.h"
+}
+
+#include <vector>
+
+int main() {
+
+  long int i, j;
+  int dimension = 1000;
+  
+  /***********************************************************************/
+  int n_eigenproblems = 2;
+  // std::vector<igraph_sparsemat_t> system_mats (n_eigenproblems);
+
+  igraph_sparsemat_t A, B;
+  igraph_matrix_t vectors;
+  igraph_vector_t values;
+  igraph_arpack_options_t options;
+  
+  igraph_rng_seed(igraph_rng_default(), 42 * 42);
+  
+  igraph_sparsemat_init(&A, dimension, dimension,
+			dimension + 2*(dimension-1));
+
+  for (j=0; j<n_eigenproblems; j++) {
+    for (i=0; i<dimension; i++) {
+      igraph_sparsemat_entry(&A, i, i,
+			     igraph_rng_get_integer(igraph_rng_default(),
+						    -10, 10));
+      if (i > 0) {
+	int off_diagonal_entry = igraph_rng_get_integer(igraph_rng_default(),
+							-10, 10);
+	igraph_sparsemat_entry(&A, i-1, i, off_diagonal_entry);
+	igraph_sparsemat_entry(&A, i, i-1, off_diagonal_entry);
+      }
+    }
+  }
+  igraph_sparsemat_compress(&A, &B);
+  igraph_sparsemat_destroy(&A);
+  
+  igraph_arpack_options_init(&options);
+  options.n = dimension;
+  options.nev = 150;
+  options.ncv = 0;
+  options.which[0] = 'L';
+  options.which[1] = 'M';
+  options.mode = 1;
+  options.sigma = 3;
+  options.tol = 1e-16;
+  
+  igraph_vector_init(&values, options.nev);
+  igraph_matrix_init(&vectors, options.n, 0);
+  
+  printf("options.which = %s\n", options.which); 
+
+  igraph_arpack_storage_t storage;
+  igraph_arpack_storage_init(&storage, dimension, dimension, dimension, true);
+
+  
+  igraph_sparsemat_arpack_rssolve(&B, &options, &storage,
+				  &values, &vectors,
+				  IGRAPH_SPARSEMAT_SOLVE_LU);
+  /* if (VECTOR(values)[0] != 1.0) { return 1; } */
+  
+  igraph_vector_print(&values); 
+  
+  igraph_vector_destroy(&values);
+  igraph_matrix_destroy(&vectors);
+  igraph_sparsemat_destroy(&B);
+  igraph_arpack_storage_destroy(&storage);
+  
+  return 0;
+}
